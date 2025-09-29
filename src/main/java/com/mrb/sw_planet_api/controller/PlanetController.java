d5a152182b90913a0705260f676e0233b71369fe
rafael <messiasrafaelb@gmail.com>
2025-09-29 14:33:31 -0300

    feat(controller): add patch, update methods and refactor content negotiation, header response in @PostMapping

--- src/main/java/com/mrb/sw_planet_api/controller/PlanetController.java
+++ src/main/java/com/mrb/sw_planet_api/controller/PlanetController.java
@@ -1,18 +1,23 @@
 package com.mrb.sw_planet_api.controller;
 
+import com.mrb.sw_planet_api.dto.PlanetPatchRequest;
 import com.mrb.sw_planet_api.dto.PlanetRequest;
 import com.mrb.sw_planet_api.dto.PlanetResponse;
 import com.mrb.sw_planet_api.service.PlanetService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import java.net.URI;
 import java.util.Map;
 import static org.springframework.data.domain.Sort.Direction.ASC;
 import static org.springframework.http.HttpStatus.CREATED;
@@ -26,10 +31,15 @@ public class PlanetController {
 
     private final PlanetService service;
 
-    @PostMapping
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PlanetResponse> createPlanet(@RequestBody @Valid PlanetRequest request) {
         var planet = service.create(request);
-        return ResponseEntity.status(CREATED).body(planet);
+        URI location = URI.create("/planets/" + planet.getId());
+        return ResponseEntity.status(CREATED)
+                .header(HttpHeaders.LOCATION, location.toString())
+                .body(planet);
     }
 
     @GetMapping("/{id}")
@@ -52,10 +62,23 @@ public class PlanetController {
         return ResponseEntity.status(OK).body(planets);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, String>> deletePlanet(@PathVariable @Min(1) Long id) {
         var message = service.delete(id);
         return ResponseEntity.status(OK).body(message);
     }
 
+    @PatchMapping("/{id}")
+    public ResponseEntity<PlanetResponse> partialUpdatePlanet(@PathVariable @Min(1) Long id,
+                                                              @RequestBody @Valid @NotNull PlanetPatchRequest request) {
+        var planet = service.patch(id, request);
+        return ResponseEntity.status(OK).body(planet);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PlanetResponse> updatePlanet(@PathVariable @Min(1) Long id,
+                                                       @RequestBody @Valid @NotNull PlanetRequest request) {
+        var planet = service.update(id, request);
+        return ResponseEntity.status(OK).body(planet);
+    }
 }

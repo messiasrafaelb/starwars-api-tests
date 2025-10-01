cdc52c132ce8e8d5e4ebe8c75ed34133de9f3cff
rafael <messiasrafaelb@gmail.com>
2025-10-01 17:52:38 -0300

    feat(exception): add annotation @JsonInclude to improve response with null field values

--- src/main/java/com/mrb/sw_planet_api/exception/ErrorResponse.java
+++ src/main/java/com/mrb/sw_planet_api/exception/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.mrb.sw_planet_api.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,12 +10,12 @@ import java.util.Map;
 
 @Getter
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
     private Instant timestamp;
     private int status;
     private String error;
     private String path;
     private String message;
-    private List<String> messages;
     private Map<String, List<String>> fieldErrors;
 }
